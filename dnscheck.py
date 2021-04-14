62d70ff9423403d952b7494a1cd2f6f5d306491f
Crazyrat <tomi@crazyrat.pl>
2021-04-14 09:25:37 +0200

    Update

--- dnscheck.py
+++ dnscheck.py
@@ -1,6 +1,7 @@
 import dns.resolver
 import colorama
 from colorama import Fore, Style
+import csv
 
 print("[*] Initiating...")
 
@@ -12,16 +13,24 @@ def check(ip):
 		my_resolver.nameservers = [ip]
 		answer = my_resolver.resolve('google.com')
 	except:
-		print("[-] [",ip,"]:",Fore.GREEN,"OK")
+		print("[-] [",ip,"]:",Fore.GREEN,"OK",Style.RESET_ALL)
 	else:
-		print("[!] [",ip,"]:",Fore.RED,"public DNS resolver")
+		print("[!] [",ip,"]:",Fore.RED,"public DNS resolver",Style.RESET_ALL)
 		#print("\t",answer.rrset[1])
 		#print(answer.response)
 	return
 
-# list of IPs to check
-IPs = ["8.8.8.8", "127.0.0.1"]
+# load IPs from file
+try:
+    IPs = []
+    with open('dnscheck.txt', newline='') as file:
+        data = csv.reader(file)
+        for l in data:
+            IPs.append(l[0])
+except:
+# or prepopulate list in code
+    IPs = ["8.8.8.8", "127.0.0.1"]
 
 for IP in IPs:
-	check(IP)
+    check(IP)
 
